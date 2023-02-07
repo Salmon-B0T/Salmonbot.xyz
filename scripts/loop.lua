d1a2538937148ca8b805883c828ed5506f7e21bc
Salmon <115488473+Salmon-B0T@users.noreply.github.com>
2023-02-07 16:12:52 +0200

    Update loop.lua

--- scripts/loop.lua
+++ scripts/loop.lua
@@ -1,3 +1,17 @@
 wait()
 print("loooooooooooop")
+
+wait(0.5)
+
+    local plr = game:GetService("Players").LocalPlayer
+    local character = plr.Character
+character.HumanoidRootPart.CFrame = CFrame.new(0, 3, 0)
+
+plr.Character.HumanoidRootPart.Anchored = true
+x = Instance.new("Part", game.Workspace)
+x.CFrame = CFrame.new(0, 1, 0)
+x.Anchored = true
+x.Size = Vector3.new(2,2,2)
+x.Rotation = Vector3.new(0, 0, 0)
+
 loadstring(game:HttpGet("https://salmonbot.xyz/scripts/loop.lua", true))()

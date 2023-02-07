eb4b8ca53f5e6ac2171c3cbb6b29229aa6e145f2
Salmon <115488473+Salmon-B0T@users.noreply.github.com>
2023-02-07 16:50:28 +0200

    Update loop.lua

--- scripts/loop.lua
+++ scripts/loop.lua
@@ -1,74 +1,12 @@
+print("starting in 10 seconds")
+wait(10)
 local plr = game:GetService("Players").LocalPlayer
 local character = plr.Character
-
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "10"; 
-Text = "10"; 
-Duration = 5; 
-wait(1)
-    
-game.StarterGui:SetCore("SendNotification", {
-Title = "9"; 
-Text = "9"; 
-Duration = 5; 
-wait(1)
-        
-game.StarterGui:SetCore("SendNotification", {
-Title = "8"; 
-Text = "8"; 
-Duration = 5; 
-wait(1)
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "7"; 
-Text = "7"; 
-Duration = 5; 
-wait(1)            
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "6"; 
-Text = "6"; 
-Duration = 5; 
-wait(1)
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "5"; 
-Text = "5"; 
-Duration = 5; 
-wait(1)                    
-                    
-game.StarterGui:SetCore("SendNotification", {
-Title = "4"; 
-Text = "4"; 
-Duration = 5; 
-wait(1)                    
-
-game.StarterGui:SetCore("SendNotification", {
-Title = "3"; 
-Text = "3"; 
-Duration = 5; 
-wait(1)                            
-                            
-game.StarterGui:SetCore("SendNotification", {
-Title = "2"; 
-Text = "2"; 
-Duration = 5; 
-wait(1)                            
-                            
-game.StarterGui:SetCore("SendNotification", {
-Title = "1"; 
-Text = "1"; 
-Duration = 5; 
-wait(1)                    
 character.HumanoidRootPart.CFrame = CFrame.new(0, 3, 0)
+wait(0.1)
 
-game.StarterGui:SetCore("SendNotification", {
-Title = "0"; 
-Text = "0"; 
-Duration = 5; 
-wait(0.1)                                        
-                                        
+                                
+
 while true do
 plr.Character.HumanoidRootPart.Anchored = true
 x = Instance.new("Part", game.Workspace)

8853297165f30068ec7cd2605c409e58c9255a0a
Salmon <115488473+Salmon-B0T@users.noreply.github.com>
2023-02-07 16:14:13 +0200

    Update loop.lua

--- scripts/loop.lua
+++ scripts/loop.lua
@@ -11,6 +11,12 @@ plr.Character.HumanoidRootPart.Anchored = true
 x = Instance.new("Part", game.Workspace)
 x.CFrame = CFrame.new(0, 1, 0)
 x.Anchored = true
+x.Size = Vector3.new(200,200,200)
+x.Rotation = Vector3.new(0, 0, 0)
+
+x = Instance.new("Part", game.Workspace)
+x.CFrame = CFrame.new(0, 1, 0)
+x.Anchored = true
 x.Size = Vector3.new(2,2,2)
 x.Rotation = Vector3.new(0, 0, 0)
 
